f257a6b20b88f67cccba68a817b1d75e537ebf6f
emilg02 <!rdj33xx>
2018-06-02 22:55:48 +0300

    graphs support, show 3d graph

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login2
 from django.shortcuts import render, redirect
 
 from movie.forms import SignUpForm
-
 
 def index(request):
     return render(request, 'home.html')
@@ -19,13 +16,10 @@ def logout(request):
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-        print (form.errors)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password')
-            print (username);
-            print (raw_password);
             user = authenticate(username=username, password=raw_password)
             login2(request, user)
             return redirect('login')
@@ -44,4 +38,9 @@ def details(request):
 
 def result(request):
     return render(request, 'result.html')
+
+def graphs(request):
+    return render(request, 'graphs.html')
+
+
 
